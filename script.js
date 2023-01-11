681640db29253010f3f0ed65cdb2d2d2dddaa67b
AdrienSuro <adrien.surowiec@protonmail.com>
2023-01-11 07:43:08 +0100

    Implement basic layout

---
+++ script.js
@@ -0,0 +1,9 @@
+const playerFactory = (name) => {
+  let score = 0;
+  // où créer une variable qui va contenir les messages du type `${name} has 4 points` ???
+  return { name, currentScore };
+};
+
+const gameboardModule = (() => {
+  let 
+})();
