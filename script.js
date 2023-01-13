e445885c79d9cfc51b3fbba38ff90b907678fe57
AdrienSuro <adrien.surowiec@protonmail.com>
2023-01-13 23:05:31 +0100

    Add buttons and a playerFactory

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-const playerFactory = (name) => {
+const playerFactory = (name, symbol) => {
   let score = 0;
-  // où créer une variable qui va contenir les messages du type `${name} has 4 points` ???
-  return { name, currentScore };
+  let addPoint = function () {
+    score += 1;
+  };
+  let winnerMessage = `${name} has won the game!`;
+  return { name, score, addPoint, winnerMessage };
 };
 
-const gameboardModule = (() => {
-  let 
+let startNewGame = function() {
+  let playerName = prompt("What's your name?");
+  let playerSymbol = prompt("Choose a symbol. Type the letter X or the letter O.");
+  let player = playerFactory(playerName, playerSymbol);
+  console.log(player);
+}
+
+document.getElementById('startButton').addEventListener('click', startNewGame);
+
+let gameboardModule = (function () {
+  const createGameboard = function () {
+    let gameboardArray = [];
+    gameboardArray.length = 9;
+    for (let i = 0; i < gameboardArray.length; i++) {
+      gameboardArray[i] = i;
+    }
+    console.log(gameboardArray);
+  };
+
+  function logGameboard() {
+    createGameboard();
+  }
+
+  return {
+    returnGameboard: function () {
+      logGameboard();
+    },
+  };
 })();
+
+// DOM Manipulation :
+const buttonGameboard = document.getElementById("buttonGameboard");
+buttonGameboard.addEventListener("click", gameboardModule.returnGameboard);
